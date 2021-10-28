b1dfeb3651724943540527916a2900f225f50f2d
Casper Andresen <casperb.andresen@gmail.com>
2021-10-28 15:29:45 +0200

    attempt

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("express")();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`sever is running on port ${port}`));
 
