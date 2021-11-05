68849dcd3deaff30fe41e3e7c2b307659ac1812a
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2021-11-05 12:47:53 +0100

    recentlyFeatured added

--- server.js
+++ server.js
@@ -89,6 +89,15 @@ const playlists = [
   },
 ];
 
+const recentlyFeatured = [
+  {
+    id: 1,
+    name: "Mariah Carey",
+    coverArt: "https://www.ladbible.com/cdn-cgi/image/width=720,quality=70,format=jpeg,fit=pad,dpr=1/https%3A%2F%2Fs3-images.ladbible.com%2Fs3%2Fcontent%2F7a640f655f9f156e5ed19f58d6e32131.png",
+    type: "Artist"
+  }
+]
+
 app.get("/songs", (req, res) => {
   res.status(200).send({
     songs,
@@ -98,5 +107,11 @@ app.get("/songs", (req, res) => {
 app.get("/playlists", (req, res) => {
   res.status(200).send({
     playlists,
+  });
+});
+
+app.get("/recentlyFeatured", (req, res) => {
+  res.status(200).send({
+    recentlyFeatured,
   });
 });
